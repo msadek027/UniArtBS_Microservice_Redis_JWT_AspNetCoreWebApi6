45d6b0a348e00b5516bc066b8f7168c3b786a40e
msadek027 <msadek027@yahoo.com>
2025-01-01 16:44:30 +0600

    commit01012025_05

--- Security/obj/Debug/net6.0/Security.AssemblyInfo.cs
+++ Security/obj/Debug/net6.0/Security.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Security")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3d95e31c0fb12395275f93bb6683090d5c7ebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2489d0934f28765e929e37d5155af6a5ac596750")]
 [assembly: System.Reflection.AssemblyProductAttribute("Security")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Security")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
