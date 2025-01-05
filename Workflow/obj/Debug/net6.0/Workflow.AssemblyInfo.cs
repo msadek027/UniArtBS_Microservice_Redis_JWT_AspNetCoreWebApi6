987e60d776b5a9bad0fd44be464e6c687afe9177
msadek027 <msadek027@yahoo.com>
2025-01-05 17:13:42 +0600

    commit05012025_05

--- Workflow/obj/Debug/net6.0/Workflow.AssemblyInfo.cs
+++ Workflow/obj/Debug/net6.0/Workflow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2489d0934f28765e929e37d5155af6a5ac596750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d6b0a348e00b5516bc066b8f7168c3b786a40e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workflow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workflow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
